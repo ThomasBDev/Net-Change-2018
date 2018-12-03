64da25dd3ed8063ff1e5c34ae7d27523420d9d0d
Rick Goossens <r.goossens2@students.uu.nl>
2018-12-03 16:44:57 +0100

    WiP voor Initialisatie en Recompute. Werkt vgm bijna, maar er moet sws nog iets geschreven worden om de mydist af te handelen.

--- MultiClientServer/Program.cs
+++ MultiClientServer/Program.cs
@@ -10,6 +10,8 @@ namespace MultiClientServer
         static public int MijnPoort;
         static public Dictionary<int, Connection> Buren = new Dictionary<int, Connection>();
         static public Dictionary<int, int> Du = new Dictionary<int, int>();
+        static public Dictionary<Tuple<int, int>, int> Ndis = new Dictionary<Tuple<int, int>, int>(); 
+        static public Dictionary<int, int> Nb = new Dictionary<int, int>();
         static Input input = new Input();
         
         static void Main(string[] args)
@@ -19,15 +21,24 @@ namespace MultiClientServer
             Console.Title = "NetChange " +  args[0];
             new Server(MijnPoort);
 
+            Du.Add(MijnPoort, 0);
+            Nb.Add(MijnPoort, MijnPoort);
             for (int t = 1; t < args.Length; t++)
             {
-                int anderePoort = int.Parse(args[t]);
-                if (MijnPoort < anderePoort)
+                int AnderePoort = int.Parse(args[t]);
+                if (MijnPoort < AnderePoort)
                 {
-                    Buren.Add(anderePoort, new Connection(anderePoort));
-                    Du.Add(anderePoort, 1);
-                    Console.WriteLine("Start verbinding gemaakt naar " + anderePoort);
+                    Buren.Add(AnderePoort, new Connection(AnderePoort));
+                    Ndis.Add(new Tuple<int, int>(AnderePoort, AnderePoort), 1);
+                    Du.Add(AnderePoort, 1);
+                    Nb.Add(AnderePoort, AnderePoort); //pref neighbour; (nb, destination)
+                    Console.WriteLine("Start verbinding gemaakt naar " + AnderePoort);
                 }
+            }
+
+            foreach (KeyValuePair<int, Connection> buur in Buren)
+            {
+                // send Mydist MijnPoort 0 to buur
             }
 
             //Na de initialisatie.
@@ -63,29 +74,29 @@ namespace MultiClientServer
                 }
                 else
                 {
-                    int anderePoort = int.Parse(input[1]);
+                    int AnderePoort = int.Parse(input[1]);
 
                     switch (messageType)
                     {
                         case "B":
                             string bericht = input[2];
-                            if (!Buren.ContainsKey(anderePoort))
-                                Console.WriteLine("We hebben geen verbinding naar " + anderePoort + " om een bericht over te kunnen sturen");
+                            if (!Buren.ContainsKey(AnderePoort))
+                                Console.WriteLine("We hebben geen verbinding naar " + AnderePoort + " om een bericht over te kunnen sturen");
                             else
-                                Buren[anderePoort].Write.WriteLine("Van " + MijnPoort + " hebben we een bericht gekregen: " + bericht);
+                                Buren[AnderePoort].Write.WriteLine("Van " + MijnPoort + " hebben we een bericht gekregen: " + bericht);
                             break;
                         //Verbining wordt nog beide kanten op gemaakt.
                         case "C":
-                            if (Buren.ContainsKey(anderePoort))
-                                Console.WriteLine("We hebben al een verbinding naar " + anderePoort);
+                            if (Buren.ContainsKey(AnderePoort))
+                                Console.WriteLine("We hebben al een verbinding naar " + AnderePoort);
                             else
                             {
                                 try
                                 {
                                     // Leg verbinding aan (als client)
-                                    Buren.Add(anderePoort, new Connection(anderePoort));
-                                    Du.Add(anderePoort, 1);
-                                    Console.WriteLine("Vebinding gemaakt naar " + anderePoort);
+                                    Buren.Add(AnderePoort, new Connection(AnderePoort));
+                                    Du.Add(AnderePoort, 1);
+                                    Console.WriteLine("Vebinding gemaakt naar " + AnderePoort);
                                 }
                                 catch(System.Net.Sockets.SocketException)
                                 {
@@ -95,12 +106,12 @@ namespace MultiClientServer
                             break;
                         //Verbinding wordt alleen vanaf deze kant verwijderd.
                         case "D":
-                            if (!Buren.ContainsKey(anderePoort))
+                            if (!Buren.ContainsKey(AnderePoort))
                                 Console.WriteLine("We hebben geen verbinding naar deze node om te verbreken");
                             else
                             {
-                                Buren.Remove(anderePoort);
-                                Console.WriteLine("Verbinding naar " + anderePoort + " verbroken");
+                                Buren.Remove(AnderePoort);
+                                Console.WriteLine("Verbinding naar " + AnderePoort + " verbroken");
                                 Console.WriteLine("Buren na verwijderen:");
                                 for (int t = 1; t < args.Length; t++)
                                     if (Buren.ContainsKey(int.Parse(args[t])))
@@ -113,6 +124,42 @@ namespace MultiClientServer
                             break;
                     }
                 }
+            }
+        }
+        public void Recompute(int Destination)
+        {
+            int oldDu = Du[Destination];
+            if (MijnPoort == Destination)
+            {
+                Du[MijnPoort] = 0;
+                Nb[MijnPoort] = MijnPoort;
+            }
+            else
+            {
+                int smallestNdis = 3;
+                int newPrefNb = Nb[Destination];
+                foreach (KeyValuePair<int, Connection> buur in Buren)
+                    if (Ndis[new Tuple<int, int>(buur.Key, MijnPoort)] < smallestNdis)
+                    {
+                        smallestNdis = Ndis[new Tuple<int, int>(buur.Key, Destination)];
+                        newPrefNb = buur.Key;
+                    }
+                int distance = 1 + smallestNdis;
+                if (distance < 3)
+                {
+                    Du[Destination] = distance;
+                    Nb[Destination] = newPrefNb;
+                }
+                else
+                {
+                    Du[Destination] = 3;
+                    Nb[Destination] = 0;
+                }
+            }
+            if (Du[Destination] != oldDu)
+            {
+                //send myDist again with new Du to all nb
+                //otherwise just stop
             }
         }
     }
